449e69765971a5fd348d063c4b6bedec066e0c9a
Shaquib <4032_20shaquib@gc11.ac.in>
2023-03-03 20:29:02 +0530

    new commit

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -42,7 +42,9 @@ const HeroBanner = () => {
                     onKeyUp={searchQueryHandler} 
                     onChange={(e)=>setQuery(e.target.value)}
                     />
-                    <button>Search</button>
+                    <button  onClick={() => navigate(`/search/${query}`)}>
+                        Search
+                    </button>
                 </div>
             </div>
         </div>
